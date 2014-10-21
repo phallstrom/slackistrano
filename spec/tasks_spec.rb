9fc42ff0b3f37585a0f29c6f3b79afbf78517313
James Lafa <jaaames.lf@gmail.com>
2014-10-21 18:19:22 +0200

    Add slackbot integration

--- spec/tasks_spec.rb
+++ spec/tasks_spec.rb
@@ -40,14 +40,15 @@ describe Slackistrano do
     it "calls Slackistrano.post with all the right arguments on slack:deploy:#{stage}" do
       set "slack_run_#{stage}".to_sym, ->{ true }
       set :slack_team,         ->{ 'team' }
-      set :slack_token,        ->{ 'token' } 
-      set :slack_channel,      ->{ 'channel' } 
+      set :slack_token,        ->{ 'token' }
+      set :slack_channel,      ->{ 'channel' }
       set :slack_icon_url,     ->{ 'http://icon.url' }
-      set :slack_icon_emoji,   ->{ ':emoji:' } 
-      set "slack_msg_#{stage}".to_sym, ->{ 'text message' } 
+      set :slack_icon_emoji,   ->{ ':emoji:' }
+      set "slack_msg_#{stage}".to_sym, ->{ 'text message' }
       expect(Slackistrano).to receive(:post).with(
         team: 'team',
         token: 'token',
+        via_slackbot: false,
         payload: {
           channel: 'channel',
           username: 'Slackistrano',
