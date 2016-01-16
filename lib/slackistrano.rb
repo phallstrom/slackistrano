a1f43c50347afd1ddc8385adffbf0fcee45213c7
Philip Hallstrom <philip@pjkh.com>
2016-01-16 09:35:11 -0800

    adjust error reporting if HTTP call fails before reaching Slack API.

--- lib/slackistrano.rb
+++ lib/slackistrano.rb
@@ -16,8 +16,8 @@ module Slackistrano
       post_as_webhook(team: team, token: token, webhook: webhook, payload: payload)
     end
   rescue => e
-    puts "There was an error notifying Slack."
-    puts e.inspect
+    error "There was an error notifying Slack."
+    error e.inspect
   end
 
   #
