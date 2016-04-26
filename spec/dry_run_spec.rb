dcf7377e195a33f3aeedade617e7a5375b4e39c1
machupicchubeta <machupicchubeta@gmail.com>
2016-04-26 15:58:17 +0900

    Wipe out warnings from rubocop

--- spec/dry_run_spec.rb
+++ spec/dry_run_spec.rb
@@ -1,12 +1,11 @@
 require 'spec_helper'
 
 describe Slackistrano do
-  %w[updating reverting updated reverted failed].each do |stage|
+  %w(updating reverting updated reverted failed).each do |stage|
     it "does not post to slack on slack:deploy:#{stage}" do
-      set "slack_run_#{stage}".to_sym, ->{ true }
+      set "slack_run_#{stage}".to_sym, -> { true }
       expect(Slackistrano).not_to receive(:post)
       Rake::Task["slack:deploy:#{stage}"].execute
     end
   end
-
 end
