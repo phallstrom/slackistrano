2efc5beb83036e02eac8369e23e9164359982574
machupicchubeta <machupicchubeta@gmail.com>
2016-04-26 15:49:19 +0900

    Avoid `load:defaults` invocation error in the test
    Capistrano "load:defaults" task has changed to immutable.
    
    see also : https://github.com/capistrano/capistrano/commit/52b0d489d7a5523605d689e49b4c990d992e627b

--- spec/dry_run_spec.rb
+++ spec/dry_run_spec.rb
@@ -1,10 +1,6 @@
 require 'spec_helper'
 
 describe Slackistrano do
-  before(:each) do
-    Rake::Task['load:defaults'].execute
-  end
-
   %w[updating reverting updated reverted failed].each do |stage|
     it "does not post to slack on slack:deploy:#{stage}" do
       set "slack_run_#{stage}".to_sym, ->{ true }
