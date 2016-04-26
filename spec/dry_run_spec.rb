18eca16b1dfca0e1fef1e841e3e6ec505f406b17
machupicchubeta <machupicchubeta@gmail.com>
2016-04-26 19:11:05 +0900

    Assume that request and response with http on the test
    Kick in the stub!

--- spec/dry_run_spec.rb
+++ spec/dry_run_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Slackistrano do
   %w(updating reverting updated reverted failed).each do |stage|
     it "does not post to slack on slack:deploy:#{stage}" do
+      allow(Net::HTTP).to receive(:post_form).and_return double(code: '200')
       set "slack_run_#{stage}".to_sym, -> { true }
       expect(Slackistrano).not_to receive(:post)
       Rake::Task["slack:deploy:#{stage}"].execute
