985dedb5cece0fd79a8f1b9a7152e58d6da39225
machupicchubeta <machupicchubeta@gmail.com>
2016-04-26 15:47:58 +0900

    Use Capistrano helper method `dry_run?`
    Goodbye legend hack.
    
    see also : https://github.com/capistrano/capistrano/commit/38d8d6d2c8485f1b5643857465b16ff01da57aff

--- spec/dry_run_spec.rb
+++ spec/dry_run_spec.rb
@@ -8,7 +8,6 @@ describe Slackistrano do
   %w[updating reverting updated reverted failed].each do |stage|
     it "does not post to slack on slack:deploy:#{stage}" do
       set "slack_run_#{stage}".to_sym, ->{ true }
-      expect(Capistrano::Configuration.env.send(:config)).to receive(:[]).with(:sshkit_backend).and_return(SSHKit::Backend::Printer)
       expect(Slackistrano).not_to receive(:post)
       Rake::Task["slack:deploy:#{stage}"].execute
     end
